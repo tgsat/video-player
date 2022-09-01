0a1bbfecfa25ead3ea05456f01bdc0857e6d1b80
tgsat <satrio.pragma@gmail.com>
2022-09-01 19:21:33 +0700

    Initial Commit

---
+++ video player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  video player
+//
+//  Created by Teguh Satrio on 9/1/22.
+//  Copyright © 2022 tgsatt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
